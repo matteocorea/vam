001a1b68f8656c719ca91721aa364bf2153b2194
Matteo Corea <matteo.corea+git@gmail.com>
2019-06-22 18:18:16 +0200

    Add documentation to easy solution

--- easy/easy.py
+++ easy/easy.py
@@ -3,12 +3,20 @@ import urllib2
 import json
 from pymongo import MongoClient
 
-if (len(sys.argv) != 2):
-        print "Usage: {} streamid".format(sys.argv[0])
-        exit(1)
-stream_id = sys.argv[1]
+def main():
+    """Read all the data from a remote dataset, and save it into a db
     
-data = urllib2.urlopen("https://api.ona.io/api/v1/data/{}".format(stream_id)).read()
-jsondata = json.loads(data)
-client = MongoClient()
-client.testdb.easycollection.insert_many(jsondata)
+    This easy solution is anyway parameterized on the dataset id, so that
+    the solution can be reused as needed for other datasets. The database host
+    and ports could also be included in the parameters, if needed."""
+    if (len(sys.argv) != 2):
+            print "Usage: {} streamid".format(sys.argv[0])
+            exit(1)
+    stream_id = sys.argv[1]
+        
+    data = urllib2.urlopen("https://api.ona.io/api/v1/data/{}".format(stream_id)).read()
+    jsondata = json.loads(data)
+    client = MongoClient()
+    client.testdb.easycollection.insert_many(jsondata)
+    
+main()
