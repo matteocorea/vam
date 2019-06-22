7b7785e9ed67a934bc16c81b207e65f67203e70a
Matteo Corea <matteo.corea+git@gmail.com>
2019-06-23 01:39:39 +0200

    VimCrypt~01!¶6aÀ§ÃyÑIðÕX#v½#ÀÛ׉ ·ÓíٕE?ÎO ¼QÅ7X좚Ðj$	Òù¶KÕª3³¥ò°[%¥³¥Ðb'ÛÌuîv¯[þÌñë˘zIÌ-yCں2]äÒiµNt[üÖCz¬¡VRÌ!Úû!GqWn*47%δï®<Ôañ¬Ñm½ò õ\ë¶ç7Ô0,Od|P¦r>6iö]YN㏊²K.±ñiäÖÀ÷Ð1EKy§Kr]{ë-ae1¯ñÿú݇CòԭOҰü´\¶è]}&­ú7+ý!)TJ°\c"mçãrÔUAªG8§}ÁÙ5zø{ׂ&ø>»}¢@&zméãBM¨Û͑T¥ñ,Û{eÙz(eȞ

---
+++ hard/scheduler/main.py
@@ -0,0 +1,41 @@
+from time import sleep
+
+"""Proof of concept for a simple scheduler.
+
+This provides a minimal skeleton for a system able to schedule polling
+and processing workloads on a number of streams.
+"""
+
+def schedule_work_for_stream(stream):
+    """Schedule work for a single stream.
+    
+    In the current proof of concept implementation this method is just
+    printing some message. In the actual implementation it should send
+    a SQS message to a given queue, containing the id of the stream to
+    be scheduled.
+    """
+    print (f'We need to poll stream #{stream}')
+    
+def get_streams_to_poll():
+    """Retrieve the list of streams we need to subscribe to.
+    
+    This currently returns a simple proof of concept list. In a proper
+    implementation this should get the list of streams from an external
+    file (so that it's easy to ask the system to subscribe to 100s of
+    streams).
+    
+    :return: List of stream id to be processed.
+    """
+    return [185260, 1234, 5423, 6534, 9345]
+    
+def main():
+    poll_frequency_seconds = 5   # This should be part of the configuration
+    streams = get_streams_to_poll()
+    
+    while(True):
+        for stream in streams:
+            schedule_work_for_stream(stream)
+        sleep(poll_frequency_seconds)
+        
+if __name__ == '__main__':
+    main()
